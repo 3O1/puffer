97a803c2bccc70f3778cb4765371cd5d1188bd44
3O1 <jamesbrandonedwards301@gmail.com>
2022-11-24 04:57:11 -0500

    Updated video size

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,13 +43,8 @@ const Home: NextPage = () => {
       <main>
         <div className="flex items-center justify-center h-screen">
           <div className="">
-            <video autoPlay loop muted playsInline>
-              <source
-                src="puffer.mp4"
-                type="video/mp4"
-                width={50}
-                height={50}
-              />
+            <video autoPlay loop muted playsInline height={200} width={200}>
+              <source src="puffer.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
